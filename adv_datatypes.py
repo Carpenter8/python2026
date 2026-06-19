424b805e101c780ab70cdf1f2c609860abd0d288
CARPENTER VINEET <vineetcarpenter123@gmail.com>
2026-06-19 08:16:59 +0530

    update_2026

---
+++ adv_datatypes.py
@@ -0,0 +1,23 @@
+# python list..
+a=[1,'vineet',6.35]
+print(a)
+print(type(a)) # double value of repeat ..
+
+
+# pythin tuple..
+a=(1,'vineet',3.45)
+print(a)
+print(type(a)) #double value of repeat ..
+
+
+#set
+a={1,'vineet',9.0}
+print(a)
+print(type(a)) # not double value no repated ..
+
+# python dictionary..
+a={'age':21,}
+print(a)
+print(type(a))
+print(a.get('age')) # key same use knhi kar skte hai ...  
+
--- index.py
+++ index.py
@@ -2,3 +2,4 @@ print('hello world ')
 print('ankit hello world')
 a="vineet"
 print(a)
+print(type(a))
