71786f1dc5bf46d04aa03edc9e83f0e9851c227a
CARPENTER VINEET <vineetcarpenter123@gmail.com>
2026-06-21 21:31:59 +0530

    update

---
+++ dict.py
@@ -0,0 +1,25 @@
+#jay ganesh 
+#date:- 21-06-2026
+'''
+var={}
+print(type(var))
+
+var=dict()
+print(type(var))
+
+empty set ka two method hai 
+'''
+
+var={'Name':'vineet','age':21,'username':'vineet'}
+print(type(var))
+print(var)
+#value dulpicate  hoga par keys is not same keys...
+
+print(var['Name'])
+print(var['age'])
+
+# new dict
+#pop method(remove)
+var={'Name':'vineet','age':21,'username':'vineet','password':'vineeet@123'}
+var.pop("Name")
+print(var)
