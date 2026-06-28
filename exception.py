a80bd198805da11a1a19160dca9d70e46e217952
CARPENTER VINEET <vineetcarpenter123@gmail.com>
2026-06-28 21:30:00 +0530

    updates

--- exception.py
+++ exception.py
@@ -1,6 +1,13 @@
 a = int(input("Enter first number"))
 b = int(input("Enter Second number"))
+try:
+    c = a/b
+    print("Result:",c)
+except:
+    print("can't devide to zero")
+else:
+    print("vineet...")
 
-c = a/b
 
-print("Result:",c)
+
+
---
+++ file.py
@@ -0,0 +1,7 @@
+f = open("D:/NEW CODE 2026/python/b.file", "r")
+
+#f.write("hello/n vineet | vijay " )
+
+print(f.readline())
+
+#print("file createto read sucessfully")
