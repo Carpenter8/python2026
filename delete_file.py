28acf34de467e8588dbbc9ddf7c9cf15f3b0c712
CARPENTER VINEET <vineetcarpenter123@gmail.com>
2026-06-30 07:46:39 +0530

    update-line

---
+++ delete_file.py
@@ -0,0 +1,8 @@
+import os 
+if os.path.exists("D:\NEW CODE 2026\python\A.text"):
+    os.remove("D:\NEW CODE 2026\python\A.text")
+
+    print("file Delected successfully ")
+
+else:
+    print("file is not available ")
--- file.py
+++ file.py
@@ -1,7 +1,27 @@
-f = open("D:/NEW CODE 2026/python/b.file", "r")
+'''f = open("D:\NEW CODE 2026\python\A.text", "r")
+print(f.readlines())'''
+#handling 
+'''
+try:
+    f = open("D:\\NEW CODE 2026\\python\\b.txt", "r")
+     
+    print(f.readlines())
+except:
+    print("file not available ....! plz create file ")
 
-#f.write("hello/n vineet | vijay " )
+else:
+    f.close()
+    print("File closed")
 
-print(f.readline())
-
-#print("file createto read sucessfully")
+'''
+    #copy 
+try:
+    with open ("D:\NEW CODE 2026\python\A.text/A.text") as f2:
+        with open ("D:\NEW CODE 2026\python\A.text/B.text") as f3 :
+             for i in f2:
+                 f3.write(i)
+except:
+     print("file not available ....! plz create file ")
+else:
+    f2.close()
+    print("File closed")
